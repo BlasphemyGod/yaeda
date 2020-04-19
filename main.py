1d812a787e1605a87fd97cc87ecaf00197210b12
Aqua <sergpolevoi@yandex.ru>
2020-04-19 05:50:57 +0300

    stable v0.1

---
+++ main.py
@@ -0,0 +1,9 @@
+from app import app
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
+
+import asyncio
+
+
+if __name__ == '__main__':
+    asyncio.run(serve(app, Config()))
